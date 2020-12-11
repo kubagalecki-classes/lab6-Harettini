310983ad2e25ba1a85a56f57cde04ee1b1d46503
Harettini <trol199717@o2.pl>
2020-12-11 12:02:48 +0000

    spróbujmy

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -9,7 +9,11 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
-
+    std::for_each(people.cbegin(), people.cend(), [&](Human HU) {
+        HU.birthday();
+        ret_v.push_back(HU.isMonster() ? 'n' : 'y');
+    });
+    std::reverse(ret_v.begin(), ret_v.end());
     // Twoja implementacja tutaj
 
     return ret_v;
