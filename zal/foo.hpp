4301a2caf10c645fb8eab25f2a0cf3a8fd4c8ce7
Harettini <trol199717@o2.pl>
2020-12-11 12:20:03 +0000

    trzecia próba

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -12,8 +12,7 @@ std::vector< char > foo(std::list< Human >& people)
     std::for_each(people.cend(), people.cbegin(), [&](const Human& HU) {
         ret_v.push_back(HU.isMonster() ? 'n' : 'y');
     });
-    std::transform(
-        people.cbegin(), people.cend(), people.cbegin(), [](Human& HU) { HU.birthday(); });
+    std::for_each(people.begin(), people.end(), [](Human& HU) { HU.birthday(); });
     //   std::reverse(ret_v.begin(), ret_v.end());
     // Twoja implementacja tutaj
 
