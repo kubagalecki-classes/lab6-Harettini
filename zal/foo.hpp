1e653b09ca9c5d499937f480035128a865860559
Harettini <trol199717@o2.pl>
2020-12-11 12:38:56 +0000

    a może teraz?

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -9,11 +9,10 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
-    std::for_each(people.rbegin(), people.rend(), [&](Human& HU) {
-        HU.birthday();
-        ret_v.push_back(HU.isMonster() ? 'n' : 'y');
+    std::transform(people.crbegin(), people.crend(), ret_v.begin(), [](const Human& HU) {
+        return HU.isMonster() ? 'n' : 'y';
     });
-    // std::for_each(people.begin(), people.end(), [](Human& HU) {  });
+    std::for_each(people.begin(), people.end(), [](Human& HU) { HU.birthday(); });
     //   std::reverse(ret_v.begin(), ret_v.end());
     // Twoja implementacja tutaj
 
