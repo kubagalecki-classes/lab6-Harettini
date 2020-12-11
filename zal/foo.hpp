f0bd01e90d035f1f87c4c70050a03ecb2704ce6d
Harettini <trol199717@o2.pl>
2020-12-11 12:16:18 +0000

    druga proba

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -9,11 +9,12 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
-    std::for_each(people.cbegin(), people.cend(), [&](Human HU) {
-        HU.birthday();
+    std::for_each(people.cend(), people.cbegin(), [&](const Human& HU) {
         ret_v.push_back(HU.isMonster() ? 'n' : 'y');
     });
-    std::reverse(ret_v.begin(), ret_v.end());
+    std::transform(
+        people.cbegin(), people.cend(), people.cbegin(), [](Human& HU) { HU.birthday(); });
+    //   std::reverse(ret_v.begin(), ret_v.end());
     // Twoja implementacja tutaj
 
     return ret_v;
